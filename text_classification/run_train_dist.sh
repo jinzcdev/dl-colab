5477782f21076254c2871486eb12b78af39161b2
jinzc <jinzcdev@gmail.com>
2026-03-22 20:37:24 +0800

    first commit

---
+++ text_classification/run_train_dist.sh
@@ -0,0 +1,24 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=. torchrun --standalone --nproc_per_node=4 text_classification/finetune_text_classification.py \
+    --task 'text-classification' \
+    --model 'damo/nlp_structbert_backbone_base_std' \
+    --train_dataset_name 'clue' \
+    --val_dataset_name 'clue' \
+    --train_subset_name 'tnews' \
+    --val_subset_name 'tnews' \
+    --train_split 'train' \
+    --val_split 'validation' \
+    --first_sequence 'sentence' \
+    --label label \
+    --preprocessor 'sen-cls-tokenizer' \
+    --use_model_config True \
+    --max_epochs 10 \
+    --per_device_train_batch_size 64 \
+    --per_device_eval_batch_size 64 \
+    --eval_interval 50 \
+    --eval_strategy by_step \
+    --work_dir './structbert_multi_gpu' \
+    --train_data_worker 0 \
+    --eval_data_worker 0 \
+    --lr 1e-5 \
+    --eval_metrics 'seq-cls-metric' \
+    --launcher 'pytorch'
